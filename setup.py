03c60176fca4cd20a6fe5da52336bacaff4a3b78
davidharvey1986 <davidharvey1986@googlemail.com>
2019-02-19 13:59:59 +0100

    Accidental rm

---
+++ setup.py
@@ -0,0 +1,39 @@
+import sys,os,string,glob,subprocess
+
+from setuptools import setup,Extension
+from setuptools.command.build_ext import build_ext
+from setuptools.command.install import install
+
+import numpy
+
+long_description = """\
+This suite of scripts uses tinytim to produce a pixellised version of 
+the ACS PSF
+"""
+#python setup.py register -r pypi
+#sudo python setup.py sdist upload -r pypi
+
+version='0.0.1'
+packages = ['pyTinyTim']
+package_dir = {'pyTinyTim':'src'}
+    
+package_data = {'pyTinyTim': ['lib/IDL/*',\
+                              'lib/cCode/*']}
+    
+INCDIRS=['.']
+
+setup   (       name            = "pyTinyTim",
+                version         = version,
+                author          = "David Harvey",
+                author_email    = "harvey@lorentz.leidenuniv.nl",
+                description     = "pyTinyTim module",
+                packages        = packages,
+                package_dir     = package_dir,
+                package_data    = package_data,
+                license         = 'MIT',
+                url = 'https://github.com/davidharvey1986/pyTinyTim', # use the URL to the github repo
+                download_url = 'https://github.com/davidharvey1986/pyTinyTim/archive/'+version+'.tar.gz',
+                install_requires=['pyRRG']
+        )
+
+
