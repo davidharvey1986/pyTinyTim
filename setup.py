61070ff685f175fd9e66ee747c4ff5b13b2c92b1
davidharvey1986 <davidharvey1986@googlemail.com>
2019-02-19 14:20:16 +0100

    WOrking at last

--- setup.py
+++ setup.py
@@ -15,9 +15,11 @@ the ACS PSF
 
 version='0.0.1'
 packages = ['pyTinyTim','IDL','cCode']
-package_dir = {'pyTinyTim':'src',
-               'IDL':'lib/IDL',
-               'cCode':'lib/cCode'}
+package_dir =  {'pyTinyTim':'src',
+               'IDL':'./lib/IDL',
+               'cCode':'./lib/cCode'}
+package_data=  {'IDL':'lib/IDL/*',
+               'cCode':'lib/cCode/*'}
     
     
 INCDIRS=['.']
@@ -29,6 +31,7 @@ setup   (       name            = "pyTinyTim",
                 description     = "pyTinyTim module",
                 packages        = packages,
                 package_dir     = package_dir,
+                package_data     = package_data,
                 license         = 'MIT',
                 url = 'https://github.com/davidharvey1986/pyTinyTim', # use the URL to the github repo
                 download_url = 'https://github.com/davidharvey1986/pyTinyTim/archive/'+version+'.tar.gz',
